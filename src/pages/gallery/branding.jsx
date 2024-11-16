b554f9c9f9d6284b4fd450a0d95808b22e1a1174
Ruman-Islam <rumanislam48@gmail.com>
2024-11-16 16:41:06 +0600

    done

--- src/pages/gallery/branding.jsx
+++ src/pages/gallery/branding.jsx
@@ -1,7 +1,7 @@
 import CloseIcon from "@mui/icons-material/Close";
 import {
   Box,
-  // Button,
+  Button,
   Dialog,
   IconButton,
   Skeleton,
@@ -17,333 +17,27 @@ import { galleryNavButtons } from "../../constants/gallery";
 import useQueryParameter from "../../hooks/useQueryParameter";
 import useTracking from "../../hooks/useTracking";
 import { useGetGalleryImageQuery } from "../../services/features/gallery/galleryApi";
-import { getImgUrl } from "../../utils/getImgUrl-utility";
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
-
-const images = [
-  {
-    images: [
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_01.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_02.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_03.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_04.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_05.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_06.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_07.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_08.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_09.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_10.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_11.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_12.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_13.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_14.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_15.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_16.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_17.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_18.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_19.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/EN-ESPÍRITU-Y-VERDAD-branding/churchlogo_en_espíritu_y_verdad_branding_design_20.jpg",
-        styles: "",
-      },
-    ],
-  },
-  {
-    images: [
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_02.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_03.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_04.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_05.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_06.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_07.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_08.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_09.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_10.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_11.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_12.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_13.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/christ-message-branding-design/churchlogo_christ_message_branding_design_14.jpg",
-        styles: "",
-      },
-    ],
-  },
-  {
-    images: [
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_01.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_02.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_03.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_04.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_05.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_06.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_07.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_08.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_09.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_10.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_11.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_12.jpg",
-        styles: "border-b",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_13.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/pavilion-church/churchlogo_pavilion_church_branding_design_14.jpg",
-        styles: "",
-      },
-    ],
-  },
-  {
-    images: [
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_01.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_02.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_03.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_04.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_05.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_06.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_07.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_08.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_09.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_10.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_11.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_12.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_13.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_14.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_15.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_16.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_17.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_18.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_19.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_20.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_21.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_22.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_23.jpg",
-        styles: "",
-      },
-      {
-        url: "image/gallery/branding/village-creek-baptist-church/churchlogo_village_creek_baptist_church_branding_design_24.jpg",
-        styles: "",
-      },
-    ],
-  },
-];
 
 export default function GalleryBrandingScreen() {
   useTracking();
   const { pathname } = useLocation();
   const [open, setOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState({});
-  const { dynamicUrl /* handleShowMoreItems */ } = useQueryParameter({
+  const { dynamicUrl, handleShowMoreItems } = useQueryParameter({
     page: 1,
     limit: 9,
-    collection: "logo-design",
+    sortBy: "serialId",
+    sortOrder: 1,
+    category: "branding",
   });
 
-  const { /* data, */ isLoading } = useGetGalleryImageQuery(dynamicUrl);
-  // const isVisibleMoreBtn = dynamicUrl.limit < data?.meta?.totalDocs;
-  // const gallery = data?.data;
+  const { data, isFetching } = useGetGalleryImageQuery(dynamicUrl);
+  const isVisibleMoreBtn = dynamicUrl.limit < data?.meta?.totalDocs;
+  const gallery = data?.data;
 
   const handleClickOpen = (img) => {
     setSelectedImage(img);
@@ -379,7 +73,7 @@ export default function GalleryBrandingScreen() {
               ))}
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-3 max-w-[1024px] w-full mx-auto gap-4">
-              {(isLoading ? Array.from(new Array(dynamicUrl.limit)) : images)
+              {(isFetching ? Array.from(new Array(dynamicUrl.limit)) : gallery)
                 ?.slice(0, dynamicUrl.limit)
                 .map((d, i) =>
                   d ? (
@@ -387,7 +81,7 @@ export default function GalleryBrandingScreen() {
                       <img
                         data-aos="flip-left"
                         data-aos-duration={`${i + 1 * 5}00`}
-                        src={getImgUrl(d?.images[0]?.url)}
+                        src={d?.urls[0].url}
                         className="w-full h-full object-cover rounded-md hover:cursor-pointer border"
                       />
                     </div>
@@ -397,17 +91,17 @@ export default function GalleryBrandingScreen() {
                 )}
             </div>
 
-            {/* {isVisibleMoreBtn ? (
+            {isVisibleMoreBtn ? (
               <div className="flex justify-center">
                 <Button
                   onClick={handleShowMoreItems}
                   className="bg-primary hover:bg-brand__black__color rounded-full px-6 font-brand__font__600"
                   variant="contained"
                 >
-                  Load More
+                  {isFetching ? "Loading..." : "Load More"}
                 </Button>
               </div>
-            ) : null} */}
+            ) : null}
           </div>
         </section>
       </Layout>
@@ -439,10 +133,15 @@ export default function GalleryBrandingScreen() {
 
           <Box className="max-w-[1280px] mx-auto px-2 md:px-0">
             <Box className="flex flex-col items-center">
-              {selectedImage?.images?.length &&
-                selectedImage?.images.map((d, i) => (
-                  <Box key={i} className={`w-full ${d?.styles}`}>
-                    <img src={getImgUrl(d?.url)} className="w-full" />
+              {selectedImage?.urls?.length &&
+                selectedImage?.urls.map((d, i) => (
+                  <Box
+                    key={i}
+                    className={`w-full ${
+                      selectedImage.showBorder ? "border-b" : "border-b-0"
+                    }`}
+                  >
+                    <img src={d?.url} className="w-full" />
                   </Box>
                 ))}
             </Box>
