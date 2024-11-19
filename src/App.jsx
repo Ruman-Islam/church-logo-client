f883ff8fb1dfcdd9d800d44b6d7e3ef1035dcfdd
Ruman-Islam <rumanislam48@gmail.com>
2024-11-20 02:24:18 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-// import AOS from "aos";
-// import { Fragment } from "react";
-// import { Toaster } from "react-hot-toast";
-// import JumpToTopBtn from "./components/common/JumpToTopBtn";
-// import Routers from "./components/Routers";
+import AOS from "aos";
+import { Fragment } from "react";
+import { Toaster } from "react-hot-toast";
+import JumpToTopBtn from "./components/common/JumpToTopBtn";
+import Routers from "./components/Routers";
 
-// AOS.init({
-//   once: true,
-// });
-
-// function App() {
-//   return (
-//     <Fragment>
-//       <Routers />
-//       <JumpToTopBtn />
-//       <Toaster position="bottom-right" />
-//     </Fragment>
-//   );
-// }
-
-// export default App;
+AOS.init({
+  once: true,
+});
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Fragment>
+      <Routers />
+      <JumpToTopBtn />
+      <Toaster position="bottom-right" />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,85 +1,63 @@
-// const rootElement = document.getElementById("root");
-// import {
-//   StyledEngineProvider,
-//   ThemeProvider,
-//   createTheme,
-// } from "@mui/material/styles";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
-// import "aos/dist/aos.css";
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "react-photo-view/dist/react-photo-view.css";
-// import "react-quill/dist/quill.snow.css";
-// import "react-quill/dist/quill.bubble.css";
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
-// import "slick-carousel/slick/slick-theme.css";
-// import "slick-carousel/slick/slick.css";
-// import App from "./App.jsx";
-// import { env } from "./config/env.js";
-// import "./index.css";
-// import store from "./services/store.js";
-// import "./styles/cross.css";
-
-// const theme = createTheme({
-//   components: {
-//     MuiPopover: {
-//       defaultProps: {
-//         container: rootElement,
-//       },
-//     },
-//     MuiPopper: {
-//       defaultProps: {
-//         container: rootElement,
-//       },
-//     },
-//     MuiDialog: {
-//       defaultProps: {
-//         container: rootElement,
-//       },
-//     },
-//     MuiModal: {
-//       defaultProps: {
-//         container: rootElement,
-//       },
-//     },
-//   },
-// });
-
-// ReactDOM.createRoot(rootElement).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <StyledEngineProvider injectFirst>
-//           <ThemeProvider theme={theme}>
-//             <GoogleOAuthProvider clientId={env.google_client_id}>
-//               <App />
-//             </GoogleOAuthProvider>
-//           </ThemeProvider>
-//         </StyledEngineProvider>
-//       </BrowserRouter>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
 const rootElement = document.getElementById("root");
+import {
+  StyledEngineProvider,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "aos/dist/aos.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "react-photo-view/dist/react-photo-view.css";
-import "react-quill/dist/quill.bubble.css";
 import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import App from "./App.jsx";
+import { env } from "./config/env.js";
 import "./index.css";
+import store from "./services/store.js";
 import "./styles/cross.css";
+
+const theme = createTheme({
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiDialog: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiModal: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <GoogleOAuthProvider clientId={env.google_client_id}>
+              <App />
+            </GoogleOAuthProvider>
+          </ThemeProvider>
+        </StyledEngineProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
